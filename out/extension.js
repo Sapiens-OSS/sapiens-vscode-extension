c02806fbdbd7ddb8d9c529a8a7a61e2fe044ed81
nmattela <nmattela@infogroep.be>
2023-02-04 15:30:30 +0100

    Add package to gitignore

--- out/extension.js
+++ out/extension.js
@@ -231,18 +231,6 @@ async function initializeProject(info) {
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "sapiens-vscode-extension" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('sapiens-vscode-extension.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from sapiens-vscode-extension!');
-    });
-    context.subscriptions.push(disposable);
     const newProject = vscode.commands.registerCommand('sapiens-vscode-extension.newProject', () => {
         const sapiensProjectInfo = {
             path: process.env.HOME ?? "/",
